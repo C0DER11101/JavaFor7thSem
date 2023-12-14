fd4202c242f58039b8987498c5cdd5b9181aa2dd
Priyanuj Bora <while.alive.program@gmail.com>
2023-12-14 11:06:02 +0530

    practice

--- practice/test3.java
+++ practice/test3.java
@@ -1,5 +1,6 @@
 class test3 {
 	public static void main(String args[]) {
+		System.out.println("main()");
 		test3 t=new test3(n);
 	}
 
@@ -9,15 +10,18 @@ class test3 {
 	int c;
 
 	public test3(int m) {
+		System.out.println("Constructor: ");
 		System.out.println(a+","+b+","+c+","+n+","+m);
 	}
 
 	{
+		System.out.println("In simple block");
 		b=30;
 		n=20;
 	}
 
 	static{
+		System.out.println("In static simple block");
 		a=60;
 	}
 }
